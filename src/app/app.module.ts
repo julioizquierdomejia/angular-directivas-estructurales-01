a621610e32b6eecd047b343bb0a95c6e70ed8f78
Julio Izquierdo Mejia <julio.izquierdo.mejia@gmail.com>
2023-01-03 10:57:27 -0500

    inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports:[
+    
+  ]
+})
+export class PagesModule { }
